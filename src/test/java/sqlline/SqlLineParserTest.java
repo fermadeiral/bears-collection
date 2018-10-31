476b74cce6a47d983af42df2a9abbbac8384d259
fermadeiral <fer.madeiral@gmail.com>
2018-10-31 06:03:32 +0100

    Changes in the tests This commit is based on the source code from the following commit: http://github.com/snuyanzin/sqlline/commit/d3802ae99861c6828b9a544d52795e4516f3a77f The mentioned commit triggered the following Travis build: http://travis-ci.org/snuyanzin/sqlline/builds/448686306.

--- src/test/java/sqlline/SqlLineParserTest.java
+++ src/test/java/sqlline/SqlLineParserTest.java
@@ -23,11 +23,12 @@ import org.junit.Test;
 public class SqlLineParserTest {
   @Test
   public void testSqlLineParserForOkLines() {
-    DefaultParser parser = new SqlLineParser(new SqlLine())
+    final DefaultParser parser = new SqlLineParser(new SqlLine())
         .eofOnUnclosedQuote(true)
         .eofOnEscapedNewLine(true);
     Parser.ParseContext acceptLine = Parser.ParseContext.ACCEPT_LINE;
-    String[] successfulLinesToCheck = {
+    final String[] lines = {
+        // commands
         "!set",
         " !history",
         "   !scan",
@@ -35,27 +36,78 @@ public class SqlLineParserTest {
         " \n test;",
         " \n test';\n;\n';",
         "select \n 1\n, '\na\n ';",
+        // sql
+        "select 1;",
+        "select '1';",
+        "select '1' as \"asd\";",
+        "select '1' as \"a's'd\";",
+        "select '1' as \"'a's'd\n\" from t;",
+        "select '1' as \"'a'\\\ns'd\\\n\n\" from t;",
+        "select ' ''1'', ''2''' as \"'a'\\\ns'd\\\n\n\" from t;",
+        "select ' ''1'', ''2''' as \"'a'\\\"\n s'd \\\" \n \\\"\n\" from t;",
+        // not a valid sql, but from sqlline parser's point of view it is ok
+        // as there are no non-closed brackets, quotes, comments
+        // and it ends with a semicolon
+        " \n test;",
+        " \n test';\n;\n';",
+
+        "select sum(my_function(x.[qwe], x.qwe)) as \"asd\" from t;",
+        "select \n 1\n, '\na\n ';",
+        "select /*\njust a comment\n*/\n'1';",
+        "--comment \n values (';\n' /* comment */, '\"'"
+            + "/*multiline;\n ;\n comment*/)\n -- ; \n;",
+
+        // non-closed or extra brackets but commented or quoted
+        "select '1(' from dual;",
+        "select ')1' from dual;",
+        "select 1/*count(123 */ from dual;",
+        "select 2/* [qwe */ from dual;",
+        "select 2 \" [qwe \" from dual;",
+        "select 2 \" ]]][[[ \" from dual;",
+        "select 2 \" ]]]\n[[[ \" from dual;",
+        "select 2 \" \n]]]\n[[[ \n\" from dual;",
+        "select 2 \n --]]]\n --[[[ \n from dual;",
     };
-    for (String line : successfulLinesToCheck) {
+    for (String line : lines) {
       parser.parse(line, line.length(), acceptLine);
     }
   }
 
   @Test
   public void testSqlLineParserForWrongLines() {
-    DefaultParser parser = new SqlLineParser(new SqlLine())
+    final DefaultParser parser = new SqlLineParser(new SqlLine())
         .eofOnUnclosedQuote(true)
         .eofOnEscapedNewLine(true);
     Parser.ParseContext acceptLine = Parser.ParseContext.ACCEPT_LINE;
-    String[] successfulLinesToCheck = {
+    final String[] lines = {
         "!sql",
         "   !all",
         " \n select",
         " \n test ",
+        // not ended quoted line
         "  test ';",
         " \n test ';'\";",
+        // not ended with ; (existing ; is commented)
+        "select --\n\n--\n--;",
+        "select /*--\n\n--\n--;",
+        "select /* \n ;",
+        "select --\n/*\n--\n--;",
+        "select ' ''\n '' '\n /* ;",
+        // not closed quotes
+        "select ''' from t;",
+        "select ''' \n'' \n'' from t;",
+        "select \"\\\" \n\\\" \n\\\" from t;",
+        // not closed brackets
+        "select to_char(123  from dual;",
+        "select sum(count(1)  from dual;",
+        "select [field  from t;",
+        // extra brackets
+        "select to_char)123) from dual;",
+        "select count)123( from dual;",
+        "select sum)count)123(( from dual;",
+        "select sum(count)t.x)) from t;"
     };
-    for (String line : successfulLinesToCheck) {
+    for (String line : lines) {
       try {
         parser.parse(line, line.length(), acceptLine);
         Assert.fail("Missing closing quote or semicolon for line " + line);
