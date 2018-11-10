71bcb7cfee8b3a8f4f00e3eb2f1c1be6423cf959
fermadeiral <fer.madeiral@gmail.com>
2018-11-10 05:36:58 +0100

    Human patch from assembleia-virtual/back This commit is based on the source code from the following commit: http://github.com/assembleia-virtual/back/commit/e213af0d396e8269517dfebde41efd3569e92176 The mentioned commit triggered the following Travis build: http://travis-ci.org/assembleia-virtual/back/builds/453176751.

--- src/main/java/assembleiavirtual/login/LoginController.java
+++ src/main/java/assembleiavirtual/login/LoginController.java
@@ -1,8 +1,10 @@
 package assembleiavirtual.login;
 
+import assembleiavirtual.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +15,8 @@ public class LoginController {
     private LoginService loginService;
 
     @RequestMapping("/login")
-    public ResponseEntity<Void> login(String email, String passwordHash) {
-        if (loginService.login(email, passwordHash)) {
+    public ResponseEntity<Void> login(@RequestBody User user) {
+        if (loginService.login(user.getEmail(), user.getPasswordHash())) {
             return new ResponseEntity<Void>(HttpStatus.FOUND);
         }
         return new ResponseEntity<Void>(HttpStatus.UNAUTHORIZED);
