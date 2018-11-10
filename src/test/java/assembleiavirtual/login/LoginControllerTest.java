fa173975cec0188c474829d30379039b8793791c
fermadeiral <fer.madeiral@gmail.com>
2018-11-10 05:36:58 +0100

    Changes in the tests This commit is based on the source code from the following commit: http://github.com/assembleia-virtual/back/commit/e213af0d396e8269517dfebde41efd3569e92176 The mentioned commit triggered the following Travis build: http://travis-ci.org/assembleia-virtual/back/builds/453176751.

--- src/test/java/assembleiavirtual/login/LoginControllerTest.java
+++ src/test/java/assembleiavirtual/login/LoginControllerTest.java
@@ -7,12 +7,14 @@ import org.mockito.Matchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -38,10 +40,27 @@ public class LoginControllerTest extends SuperControllerHelper {
     }
 
     @Test
+    public void testWithouBody() throws Exception {
+        mockMvc.perform(get("/login")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testEmptyBody() throws Exception {
+        mockMvc.perform(get("/login")
+                .contentType(APPLICATION_JSON)
+                .content("{}"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
     public void testUnauthorized() throws Exception {
         when(loginService.login(any(String.class), any(String.class))).thenReturn(false);
 
-        mockMvc.perform(get("/login"))
+        mockMvc.perform(get("/login")
+                .contentType(APPLICATION_JSON)
+                .content("{\"email\":\"email@email.email\"}"))
                 .andExpect(status().isUnauthorized());
     }
 
@@ -49,7 +68,9 @@ public class LoginControllerTest extends SuperControllerHelper {
     public void testAuthorized() throws Exception {
         when(loginService.login(any(String.class), any(String.class))).thenReturn(true);
 
-        mockMvc.perform(get("/login"))
+        mockMvc.perform(get("/login")
+                .contentType(APPLICATION_JSON)
+                .content("{\"email\":\"email@email.email\"}"))
                 .andExpect(status().isFound());
     }
 
